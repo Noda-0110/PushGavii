c6e002e4547a73d8779d34bcb54470499398798a
Noda-0110 <hj-g20024@sist.ac.jp>
2022-11-02 14:29:48 +0900

    敵との接触判定をtriggerをcollisionに変更

--- Assets/Scripts/PlayerCon/GaviController.cs
+++ Assets/Scripts/PlayerCon/GaviController.cs
@@ -185,9 +185,7 @@ public class GaviController : MonoBehaviour
         return isGroundCheck;
     }
 
-
-    //何かに入った
-    private void OnTriggerEnter2D(Collider2D coll)
+    private void OnCollisionEnter2D(Collision2D coll)
     {
         Lifelength = LifeCount.Length - 1;
         if (coll.gameObject.tag == "Enemy")
@@ -200,6 +198,12 @@ public class GaviController : MonoBehaviour
             //プライヤーをワープ先に移動
             Player.transform.position = Worp.transform.position;
         }
+    }
+
+    //何かに入った
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+
         if (coll.gameObject.tag == "Goal")
         {
             speed = 0;
