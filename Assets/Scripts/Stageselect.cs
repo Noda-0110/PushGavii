724feb2e9ed1b4cb08ec1b5b3b6f8dfd3f9859e2
Noda-0110 <hj-g20024@sist.ac.jp>
2022-11-29 11:36:38 +0900

    ステージ管理の仕組みを修正

--- Assets/Scripts/Stageselect.cs
+++ Assets/Scripts/Stageselect.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 public class Stageselect : MonoBehaviour
 {
     public static Stageselect instance;
-
     [Header("現在のクリアしたステージ")]
-    public int clearstage;  //クリアしたステージ
+    public int clearstage1;  //クリアしたステージ
+    public int clearstage2;  //クリアしたステージ
+    public int clearstage3;  //クリアしたステージ
+    public int clearstage4;  //クリアしたステージ
+    public int clearstage5;  //クリアしたステージ
+    public int clearstage6;  //クリアしたステージ
     public GameObject Player;   //プレイヤーの位置
     public GameObject[] Worp;   //ワープ先の位置
     private int nowStage = 0;        //現在のステージ
@@ -26,20 +30,124 @@ public class Stageselect : MonoBehaviour
     {
         //最後に遊んだステージを入れる
         PlayerPrefs.SetInt("StagePlay", nowWold);
+
         //Worp[1]までは選択可能
-        clearstage = PlayerPrefs.GetInt("StageClear", 1);
+        for (int a = 0; a >= 5; a++) {
+        }
         //最初はWorp[0]の位置にガービィを置く
 
         //ステージ数取得
         Stagelength = Worp.Length - 1;
-
+        if (nowWold == 1)
+        {
+            clearstage1 = PlayerPrefs.GetInt("StageClear" + 1, 1); 
+            stagemove(1);
+        }
+        if (nowWold == 2)
+        {
+            clearstage2 = PlayerPrefs.GetInt("StageClear" + 2, 1); 
+            stagemove(2);
+        }
+        if (nowWold == 3)
+        {
+            clearstage3 = PlayerPrefs.GetInt("StageClear" + 3, 1); 
+            stagemove(3);
+        }
+        if (nowWold == 4)
+        {
+            clearstage4 = PlayerPrefs.GetInt("StageClear" + 4, 1); 
+            stagemove(4);
+        }
+        if (nowWold == 5)
+        {
+            clearstage5 = PlayerPrefs.GetInt("StageClear" + 5, 1); 
+            stagemove(5);
+        }
+        if (nowWold == 6)
+        {
+            clearstage6 = PlayerPrefs.GetInt("StageClear" + 6, 1); 
+            stagemove(6);
+        }
+        
+        Debug.Log(nowStage);
+        switch(nowWold){
+            case 1:
+                Debug.Log("現在のクリアしたステージは" + clearstage1 + "まで");
+                break;
+            case 2:
+                Debug.Log("現在のクリアしたステージは" + clearstage2 + "まで");
+                break;
+            case 3:
+                Debug.Log("現在のクリアしたステージは" + clearstage3 + "まで");
+                break;
+            case 4:
+                Debug.Log("現在のクリアしたステージは" + clearstage4 + "まで");
+                break;
+            case 5:
+                Debug.Log("現在のクリアしたステージは" + clearstage5 + "まで");
+                break;
+            case 6:
+                Debug.Log("現在のクリアしたステージは" + clearstage6 + "まで");
+                break;
+        }
+    }
+    public void stagemove(int Nowold)
+    {
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //ステージの数より先には進まない
-            if (Stagelength > nowStage && nowStage < clearstage)
+            if (Nowold == 1)
             {
-                nowStage++;
-                Player.transform.position = Worp[nowStage].transform.position;
+                //ステージの数より先には進まない
+                if (Stagelength > nowStage && nowStage < clearstage1)
+                {
+                    nowStage++;
+                    Player.transform.position = Worp[nowStage].transform.position;
+                }
+            }
+            if (Nowold == 2)
+            {
+                //ステージの数より先には進まない
+                if (Stagelength > nowStage && nowStage < clearstage2)
+                {
+                    nowStage++;
+                    Player.transform.position = Worp[nowStage].transform.position;
+                }
+            }
+            if (Nowold == 3)
+            {
+                //ステージの数より先には進まない
+                if (Stagelength > nowStage && nowStage < clearstage3)
+                {
+                    nowStage++;
+                    Player.transform.position = Worp[nowStage].transform.position;
+                }
+            }
+            if (Nowold == 4)
+            {
+                //ステージの数より先には進まない
+                if (Stagelength > nowStage && nowStage < clearstage4)
+                {
+                    nowStage++;
+                    Player.transform.position = Worp[nowStage].transform.position;
+                }
+            }
+            if (Nowold == 5)
+            {
+                //ステージの数より先には進まない
+                if (Stagelength > nowStage && nowStage < clearstage5)
+                {
+                    nowStage++;
+                    Player.transform.position = Worp[nowStage].transform.position;
+                }
+            }
+            if (Nowold == 6)
+            {
+                //ステージの数より先には進まない
+                if (Stagelength > nowStage && nowStage < clearstage6)
+                {
+                    nowStage++;
+                    Player.transform.position = Worp[nowStage].transform.position;
+                }
             }
         }
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
@@ -72,10 +180,5 @@ public class Stageselect : MonoBehaviour
                 SceneManager.LoadScene("stage0Wold");
             }
         }
- 
-
-        Debug.Log(nowStage);
-        Debug.Log("現在のクリアしたステージは" + clearstage + "まで");
     }
-
 }
