4312da3dbb1a3f272752da393ed9f8f8d3d74e27
Noda-0110 <hj-g20024@sist.ac.jp>
2022-12-15 13:18:19 +0900

    stage4にムービー追加

--- Assets/Scripts/TitleHouse0.cs
+++ Assets/Scripts/TitleHouse0.cs
@@ -7,6 +7,11 @@ public class TitleHouse0 : MonoBehaviour
 {
     private int lastplay,newmovie;
     public bool Title;
+    public bool stage4;
+    [SerializeField] private Animator stage4anim_tobira;
+    [SerializeField] private Animator stage4anim_tobira_ue;
+    [SerializeField] private Animator stage4anim_tobira_sita;
+    [SerializeField] private Animator stage4anim_tobira_temae;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,11 +33,31 @@ public class TitleHouse0 : MonoBehaviour
         }
         else
         {
-            //エンターキーでHouseへ
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (stage4 == false)
             {
+                //エンターキーでHouseへ
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    PlayerPrefs.SetInt("movie" + lastplay, 1);
+                    SceneManager.LoadScene("stage" + lastplay + "-1");
+                }
+            }
+            else if (stage4 == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    stage4anim_tobira.SetBool("PushEnter", true);
+                    stage4anim_tobira_ue.SetBool("PushEnter", true);
+                    stage4anim_tobira_sita.SetBool("PushEnter", true);
+                    stage4anim_tobira_temae.SetBool("PushEnter", true);
+                    StartCoroutine(houkai());
+                }
+            }
+            IEnumerator houkai()
+            {
+                yield return new WaitForSeconds(8);
                 PlayerPrefs.SetInt("movie" + lastplay, 1);
-                SceneManager.LoadScene("stage"+lastplay + "-1");
+                SceneManager.LoadScene("stage" + lastplay + "-1");
             }
         }
     }
