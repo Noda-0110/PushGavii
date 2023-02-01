1ef55c151def5ff53a5b50636706dd3b4aef7000
OokuboHiroki <hj-g20005@sist.ac.jp>
2023-02-01 11:11:54 +0900

    no message

--- Assets/Scenes/stage-/Scripts/PlayerCon/MoveEnemy.cs
+++ Assets/Scenes/stage-/Scripts/PlayerCon/MoveEnemy.cs
@@ -13,17 +13,30 @@ public class MoveEnemy : MonoBehaviour
 {
     public bool Die = false;
     public float speed = 0;
+    public float rspeed = 0;
     [SerializeField] private Rigidbody2D rb;
 
     GameObject Player;
     GaviController Lifescript;
     private bool Engine;
 
+    //反転
+    private bool erflg = true;
+
+    //重力反転
+    private bool egflg = false;
+    private bool egrflg = false;
+
     void Start()
     {
         Player = GameObject.Find("Chara");
         Lifescript = Player.GetComponent<GaviController>();
         Engine = false;
+        //反転をなくす
+        erflg = true;
+        // 重力反転をなくす
+        egflg = false;
+        egrflg = false;
     }
 
     void Update()
@@ -40,10 +53,110 @@ public class MoveEnemy : MonoBehaviour
             {
                 speed = 0;
             }
-            //常に動き続ける、影響受けない、段差止まる
-            rb.velocity = new Vector2(speed, rb.velocity.y);
+            if (erflg)
+            {
+                //常に左へ進み続ける、影響受けない、段差止まる
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            }
+            if (!erflg)
+            {
+                //常に左へ進み続ける、影響受けない、段差止まる
+                rb.velocity = new Vector2(rspeed, rb.velocity.y);
+                // ローカル座標基準で、現在の回転量へ加算する
+                this.transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+            }
 
+            // 元に戻す
+            if (!egrflg)
+            {
+                //常に右へ進み続ける、影響受けない、段差止まる
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                Physics2D.gravity = new Vector2(0.0f, -9.81f);
+                erflg = true;
+            }
+
+            // gflgがtrueで、grflgで反対を向く
+            if (egflg && egrflg)
+            {
+                //常に右へ進み続ける、影響受けない、段差止まる
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                Physics2D.gravity = new Vector2(0.0f, 9.81f);
+                // ローカル座標基準で、現在の回転量へ加算する
+                this.transform.rotation = Quaternion.Euler(180.0f, 180.0f, 0.0f);
+            }
+
+            // gflgがtrueで、grflgで正面を向く
+            if (egflg && !egrflg)
+            {
+                //常に右へ進み続ける、影響受けない、段差止まる
+                rb.velocity = new Vector2(rspeed, rb.velocity.y);
+                Physics2D.gravity = new Vector2(0.0f, 9.81f);
+                // ローカル座標基準で、現在の回転量へ加算する
+                this.transform.rotation = Quaternion.Euler(180.0f, 0.0f, 0.0f);
+            }
+            // gflgがtrueで、grflgで正面を向く
+            if (!egflg && egrflg)
+            {
+                //常に右へ進み続ける、影響受けない、段差止まる
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+                Physics2D.gravity = new Vector2(0.0f, -9.81f);
+                // ローカル座標基準で、現在の回転量へ加算する
+                this.transform.rotation = Quaternion.Euler(0f, 180.0f, 0.0f);
+            }
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Reverse")
+        {
+            erflg = true;
+            egrflg = true;
+        }
+
+        if (coll.gameObject.tag == "RReverse")
+        {
+            erflg = false;
+            egrflg = false;
+        }
+
+        if (coll.gameObject.tag == "Gravity")
+        {
+            //向き　重力　反転せず
+            if (egflg == false && egrflg == false)
+            {
+                egflg = true;
+                egrflg = true;
+            }
+            //向きだけ反転
+            else if (egflg == false && egrflg == true)
+            {
+                egflg = true;
+                egrflg = false;
+            }
+            //重力だけ反転
+            else if (egflg == true && egrflg == false)
+            {
+                egflg = false;
+                egrflg = true;
+            }
+            //向き　重力　反転
+            else if (egflg == true && egrflg == true)
+            {
+                egflg = false;
+                egrflg = false;
+            }
+        }
+
+        if (coll.gameObject.tag == "rGravity")
+        {
+            egflg = true;
+            egrflg = false;
+        }
+
+    }
 }
+
+        
